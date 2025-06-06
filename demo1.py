a4fe893a14eb83d3e11024f261624f4b0b41508a
niloy-cyber <nyt252465@gmail.com>
2025-06-06 22:30:21 +0600

    branch1

--- demo1.py
+++ demo1.py
@@ -1,3 +1,9 @@
 print('I am here to try something new.')
 print('I am commiting this for the first time to make some changes')
 print('I am writing this to remove')
+print("It's lokking like, I don't have to remove anything for now")
+
+
+
+
+printI('done for now')
